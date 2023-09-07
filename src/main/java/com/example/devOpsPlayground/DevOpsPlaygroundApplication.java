e9b77f509907632daf9613b2060ac9887d9a197d
hsh_519 <suk000519@naver.com>
2023-09-07 15:24:35 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/devOpsPlayground/DevOpsPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.devOpsPlayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devOpsPlayground/DevOpsPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devOpsPlayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
