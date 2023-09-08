ad223d54c630f6e9a1cb2d0c7cf4e4418167da9d
hsh_519 <suk000519@naver.com>
2023-09-08 22:58:59 +0900

    Member 클래스 수정

--- src/main/java/com/example/devOpsPlayground/member/Member.java
+++ src/main/java/com/example/devOpsPlayground/member/Member.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -12,7 +13,8 @@ import javax.persistence.Id;
 @Data
 public class Member {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int age;
